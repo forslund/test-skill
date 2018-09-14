4ff751b2fca79e09a07da26c641ed8ccfe9337d3
Åke Forslund <ake.forslund@gmail.com>
2018-09-14 12:02:23 +0200

    Update

--- __init__.py
+++ __init__.py
@@ -5,10 +5,21 @@ class TestMsk(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('msk.test.intent')
+    @intent_file_handler('test1.intent')
     def handle_msk_test(self, message):
-        self.speak_dialog('msk.test')
+        self.speak_dialog('test1')
 
+    @intent_file_handler('test2.intent')
+    def handle_msk_test2(self, message):
+        self.speak_dialog('test2')
+
+    @intent_file_handler('test3.intent')
+    def handle_msk_test3(self, message):
+        self.speak('test3')
+
+    @intent_file_handler('test4.intent')
+    def handle_msk_test4(self, message):
+        self.speak('test4')
 
 def create_skill():
     return TestMsk()
